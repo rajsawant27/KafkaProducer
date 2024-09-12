2b6aa1cece29cf9bbdbd54bd0af6d528f786c064
RAJ <rajsawant2722001@gmail.com>
2024-09-12 15:53:58 +0530

    Kafka Producer

---
+++ src/main/java/com/kafkaImpl/DeliveryBoy/config/AppConstants.java
@@ -0,0 +1,7 @@
+package com.kafkaImpl.DeliveryBoy.config;
+
+public class AppConstants {
+
+	public static final String LOCATION_TOPIC_NAME = "location-update-topic";
+
+}
---
+++ src/test/java/com/kafkaImpl/DeliveryBoy/DeliveryBoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaImpl.DeliveryBoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryBoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
